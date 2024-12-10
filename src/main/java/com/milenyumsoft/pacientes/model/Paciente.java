0e2a92fd18f2346b25adde3cdb5cb5df720c0ab5
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2024-12-09 21:23:12 -0500

    Integración de las configuraciones con la base de datos properties

--- src/main/java/com/milenyumsoft/pacientes/model/Paciente.java
+++ src/main/java/com/milenyumsoft/pacientes/model/Paciente.java
@@ -19,7 +19,7 @@ import java.util.Date;
 public class Paciente {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idPaciente;
     private String dni;
     private String nombre;
