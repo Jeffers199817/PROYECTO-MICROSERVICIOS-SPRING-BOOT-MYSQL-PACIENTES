d237aa23536ac721162eb55b6dfc1012d707b2d9
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2024-12-09 20:59:16 -0500

    Implementación del modelo, repositorio, service, para el modelo paciente

---
+++ src/main/java/com/milenyumsoft/pacientes/service/IPacienteService.java
@@ -0,0 +1,19 @@
+package com.milenyumsoft.pacientes.service;
+
+import com.milenyumsoft.pacientes.model.Paciente;
+
+import java.util.List;
+
+public interface IPacienteService {
+
+
+    public List<Paciente> getPacientes();
+
+    public void savePaciente(Paciente paciente);
+
+    public void deletePaciente(Long id);
+
+    public Paciente findPaciente(Long id);
+
+    public void editPaciente( Paciente paciente);
+}
---
+++ src/main/java/com/milenyumsoft/pacientes/service/PacienteService.java
@@ -0,0 +1,50 @@
+package com.milenyumsoft.pacientes.service;
+
+import com.milenyumsoft.pacientes.model.Paciente;
+import com.milenyumsoft.pacientes.repository.IPacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PacienteService implements IPacienteService {
+
+    @Autowired
+    private IPacienteRepository pacienteRepository;
+
+
+    @Override
+    public List<Paciente> getPacientes() {
+        return pacienteRepository.findAll();
+    }
+
+    @Override
+    public void savePaciente(Paciente paciente) {
+
+        pacienteRepository.save(paciente);
+    }
+
+    @Override
+    public void deletePaciente(Long id) {
+        pacienteRepository.deleteById(id);
+    }
+
+    @Override
+    public Paciente findPaciente(Long id) {
+        return pacienteRepository.findById(id).get();
+    }
+
+    @Override
+    public void editPaciente(Paciente paciente) {
+
+       Paciente pac =  this.findPaciente(paciente.getIdPaciente());
+       pac.setDni(paciente.getDni());
+       pac.setNombre(paciente.getNombre());
+       pac.setApellido(paciente.getApellido());
+       pac.setFechaNac(paciente.getFechaNac());
+       pac.setTelefono(paciente.getTelefono());
+
+      pacienteRepository.save(pac);
+    }
+}
