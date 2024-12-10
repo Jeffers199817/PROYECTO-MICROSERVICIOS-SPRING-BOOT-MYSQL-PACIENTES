a418372d674d98bba9014b50ce77cf75908e7c3e
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2024-12-09 20:41:27 -0500

    Inicio de proyecto Microservicios, sprint 1 configuraciones inciales

---
+++ src/main/java/com/milenyumsoft/pacientes/model/Paciente.java
@@ -0,0 +1,30 @@
+package com.milenyumsoft.pacientes.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long idPaciente;
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private Date fechaNac;
+    private String telefono;
+
+}
